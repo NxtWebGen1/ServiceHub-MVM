3f18e86917253fb4cecbd849357b399d5e5af10c
Next Web Gen <nxtwebgen1@gmail.com>
2025-03-24 03:17:09 +0500

    uncommenting  vendor redirection to vendor_login
    =

--- includes/restrict-vendor-login.php
+++ includes/restrict-vendor-login.php
@@ -21,13 +21,13 @@ add_filter('wp_authenticate_user', 'servicehub_mvm_restrict_vendor_login', 10, 2
 
 
 // Redirect vendors from wp-login.php to custom login page
-// function servicehub_mvm_redirect_vendor_login() {
-//     if (strpos($_SERVER['REQUEST_URI'], 'wp-login.php') !== false && !is_admin()) {
-//         wp_redirect(home_url('/vendor-login')); // Redirect vendors to custom login page
-//         exit;
-//     }
-// }
-// add_action('init', 'servicehub_mvm_redirect_vendor_login');  
+function servicehub_mvm_redirect_vendor_login() {
+    if (strpos($_SERVER['REQUEST_URI'], 'wp-login.php') !== false && !is_admin()) {
+        wp_redirect(home_url('/vendor-login')); // Redirect vendors to custom login page
+        exit;
+    }
+}
+add_action('init', 'servicehub_mvm_redirect_vendor_login');  
 
 
 
